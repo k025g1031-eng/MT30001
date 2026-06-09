d7e0c865fadda6b7227662f1f05f7c8390768ccf
k025g1031 <k025g1031@g.neec.ac.jp>
2026-06-09 12:27:44 +0900

    1000

--- MT30001/main.cpp
+++ MT30001/main.cpp
@@ -44,11 +44,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		Vector3 scale{ 1.2f,0.79f,-2.1f };
-		Vector3 rotate{ 0.4f, 1.43f,-0.8f };
-		Vector3 translate{ 2.7f,-4.15f,1.57f };
-		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
-		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		Matrix4x4 orthographicMatrix =
+			MakeOrthographicMatrix(-160.f, 160.f, 200.0f, 300.0f, 0.0f, 1000.0f);
+		Matrix4x4 perspectiveFovMatrix =
+			MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+		Matrix4x4 viewportMatrix =
+			MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 		///				   
 		/// ↑更新処理ここまで
 		///
--- MT30001/transform.cpp
+++ MT30001/transform.cpp
@@ -1,143 +1,76 @@
 #include"transform.h"
 #include <cmath>
 
-Matrix4x4 MakeRotateXMatrix(float radian) {
-    Matrix4x4 result = {
-        {
-            {1.0f, 0.0f,              0.0f,             0.0f},
-            {0.0f, std::cos(radian),  std::sin(radian), 0.0f},
-            {0.0f,-std::sin(radian),  std::cos(radian), 0.0f},
-            {0.0f, 0.0f,              0.0f,             1.0f}
-        }
-    };
 
-    return result;
-}
-Matrix4x4 MakeRotateYMatrix(float radian) {
-    Matrix4x4 result = {
-        {
-            { std::cos(radian), 0.0f,-std::sin(radian), 0.0f},
-            { 0.0f,             1.0f, 0.0f,             0.0f},
-            { std::sin(radian), 0.0f, std::cos(radian), 0.0f},
-            { 0.0f,             0.0f, 0.0f,             1.0f}
-        }
-    };
 
-    return result;
-}
-Matrix4x4 MakeRotateZMatrix(float radian) {
-    Matrix4x4 result = {
-        {
-            { std::cos(radian),  std::sin(radian), 0.0f, 0.0f},
-            {-std::sin(radian),  std::cos(radian), 0.0f, 0.0f},
-            { 0.0f,              0.0f,             1.0f, 0.0f},
-            { 0.0f,              0.0f,             0.0f, 1.0f}
-        }
-    };
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+   Matrix4x4 result{};
 
-    return result;
+   float cotY = 1.0f / tanf(fovY / 2.0f);
+
+   result.m[0][0] = cotY / aspectRatio;
+   result.m[1][1] = cotY;
+
+   result.m[2][2] = farClip / (farClip - nearClip);
+   result.m[2][3] = 1.0f;
+
+   result.m[3][2] =
+       -nearClip * farClip /
+       (farClip - nearClip);
+
+   return result;
 }
 
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
+{
+   Matrix4x4 result{};
+
+   result.m[0][0] =
+       2.0f / (right - left);
+
+   result.m[1][1] =
+       2.0f / (top - bottom);
+
+   result.m[2][2] =
+       1.0f / (farClip - nearClip);
+
+   result.m[3][0] =
+       (left + right) /
+       (left - right);
+
+   result.m[3][1] =
+       (top + bottom) /
+       (bottom - top);
+
+   result.m[3][2] =
+       nearClip /
+       (nearClip - farClip);
+
+   result.m[3][3] = 1.0f;
+
+   return result;
+}
+
+Matrix4x4 MakeViewportMatrix(
+    float left,
+    float top,
+    float width,
+    float height,
+    float minDepth,
+    float maxDepth) {
 
     Matrix4x4 result{};
 
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
+    result.m[0][0] = width / 2.0f;
+    result.m[1][1] = -height / 2.0f;
+    result.m[2][2] = maxDepth - minDepth;
 
-            result.m[y][x] =
-                m1.m[y][0] * m2.m[0][x] +
-                m1.m[y][1] * m2.m[1][x] +
-                m1.m[y][2] * m2.m[2][x] +
-                m1.m[y][3] * m2.m[3][x];
+    result.m[3][0] = left + width / 2.0f;
+    result.m[3][1] = top + height / 2.0f;
+    result.m[3][2] = minDepth;
 
-        }
-    }
+    result.m[3][3] = 1.0f;
 
     return result;
-}
-
-Matrix4x4 MakeIdentity4x4() { 
-    Matrix4x4 result{};    
-    for (int y = 0; y < 4; y++) { 
-        for (int x = 0; x < 4; x++) {
-            if (x == y) {
-                result.m[y][x] = 1.0f; 
-            } 
-            else {
-                result.m[y][x] = 0.0f; 
-            } 
-        } 
-    }    
-    return result; 
-}
-
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
-    Matrix4x4 result = MakeIdentity4x4();    
-    result.m[3][0] = translate.x;    
-    result.m[3][1] = translate.y;    
-    result.m[3][2] = translate.z;    
-    return result; 
-
-}
-
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) { 
-    Matrix4x4 result = MakeIdentity4x4();    
-    result.m[0][0] = scale.x;    
-    result.m[1][1] = scale.y;    
-    result.m[2][2] = scale.z;    
-    return result; 
-}
-
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) { 
-    Vector3 result;    
-    result.x = vector.x * matrix.m[0][0] + 
-        vector.y * matrix.m[1][0] + 
-        vector.z * matrix.m[2][0] + 
-        1.0f * matrix.m[3][0];   
-    
-    result.y = vector.x * matrix.m[0][1] + 
-        vector.y * matrix.m[1][1] + 
-        vector.z * matrix.m[2][1] + 
-        1.0f * matrix.m[3][1];    
-    
-    result.z = vector.x * matrix.m[0][2] + 
-        vector.y * matrix.m[1][2] + 
-        vector.z * matrix.m[2][2] + 
-        1.0f * matrix.m[3][2];    
-    
-    return result; 
-
-}
-
-Matrix4x4 MakeAffineMatrix(
-    const Vector3& scale,
-    const Vector3& rotate,
-    const Vector3& translate)
-{
-    Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-
-    Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-    Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-    Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-
-    Matrix4x4 rotateMatrix =
-        Multiply(
-            rotateXMatrix,
-            Multiply(rotateYMatrix, rotateZMatrix)
-        );
-
-    Matrix4x4 translateMatrix =
-        MakeTranslateMatrix(translate);
-
-    Matrix4x4 worldMatrix =
-        Multiply(
-            scaleMatrix,
-            Multiply(
-                rotateMatrix,
-                translateMatrix
-            )
-        );
-
-    return worldMatrix;
 }
